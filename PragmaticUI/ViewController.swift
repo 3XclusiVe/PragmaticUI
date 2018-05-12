c10ff4a8402712d4418c9999ba20a19415d09b6b
Dmitriy Malych <malllkin007@gmail.com>
2018-05-12 15:22:46 +0300

    Initial Commit

---
+++ PragmaticUI/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PragmaticUI
+//
+//  Created by Дмитрий on 12.05.18.
+//  Copyright © 2018 Дмитрий. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
