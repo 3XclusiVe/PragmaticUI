b22c35e08450c417673ce176d8e97d939efeab66
Dmitriy Malych <malllkin007@gmail.com>
2018-05-12 16:40:49 +0300

    init

--- PragmaticUI/ViewController.swift
+++ PragmaticUI/ViewController.swift
@@ -8,18 +8,176 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    // MARK: - UIViewController Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        textField.delegate = self
+        
+        view.addSubview(textField)
+        view.addSubview(button)
+        view.addSubview(label)
+        view.setNeedsUpdateConstraints()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    // MARK: - Layout Constraints
+    override func updateViewConstraints() {
+        textFieldConstraints()
+        buttonConstraints()
+        labelConstraints()
+        super.updateViewConstraints()
     }
-
-
+    
+    func textFieldConstraints() {
+        NSLayoutConstraint(
+            item: textField,
+            attribute: .centerX,
+            relatedBy: .equal,
+            toItem: view,
+            attribute: .centerX,
+            multiplier: 1.0,
+            constant: 0.0)
+            .isActive = true
+        
+        NSLayoutConstraint(
+            item: textField,
+            attribute: .width,
+            relatedBy: .equal,
+            toItem: view,
+            attribute: .width,
+            multiplier: 0.8,
+            constant: 0.0)
+            .isActive = true
+        
+        NSLayoutConstraint(
+            item: textField,
+            attribute: .top,
+            relatedBy: .equal,
+            toItem: view,
+            attribute: .bottom,
+            multiplier: 0.1,
+            constant: 0.0)
+            .isActive = true
+    }
+    
+    func buttonConstraints() {
+        // Center button in Page View
+        NSLayoutConstraint(
+            item: button,
+            attribute: .centerX,
+            relatedBy: .equal,
+            toItem: view,
+            attribute: .centerX,
+            multiplier: 1.0,
+            constant: 0.0)
+            .isActive = true
+        
+        // Set Width to be 30% of the Page View Width
+        NSLayoutConstraint(
+            item: button,
+            attribute: .width,
+            relatedBy: .equal,
+            toItem: view,
+            attribute: .width,
+            multiplier: 0.3,
+            constant: 0.0)
+            .isActive = true
+        
+        // Set Y Position Relative to Bottom of Page View
+        NSLayoutConstraint(
+            item: button,
+            attribute: .bottom,
+            relatedBy: .equal,
+            toItem: view,
+            attribute: .bottom,
+            multiplier: 0.9,
+            constant: 0.0)
+            .isActive = true
+    }
+    
+    func labelConstraints() {
+        // Center button in Page View
+        NSLayoutConstraint(
+            item: label,
+            attribute: .centerX,
+            relatedBy: .equal,
+            toItem: view,
+            attribute: .centerX,
+            multiplier: 1.0,
+            constant: 0.0)
+            .isActive = true
+        
+        // Set Width to be 80% of the Page View Width
+        NSLayoutConstraint(
+            item: label,
+            attribute: .width,
+            relatedBy: .equal,
+            toItem: view,
+            attribute: .width,
+            multiplier: 0.8,
+            constant: 0.0)
+            .isActive = true
+        
+        // Set Y Position Relative to Bottom of Page View
+        NSLayoutConstraint(
+            item: label,
+            attribute: .centerY,
+            relatedBy: .equal,
+            toItem: view,
+            attribute: .centerY,
+            multiplier: 1.0,
+            constant: 0.0)
+            .isActive = true
+    }
+    
+    
+    // MARK: - UI Actions
+    @objc func buttonPressed() {
+        // Print Hello followed by the user inputed string
+        // The exclamation point is added to unwrap the optional
+        label.text = "Hello, \(textField.text!)"
+    }
+    
+    // MARK: - Lazy Instantiation
+    lazy var textField: UITextField! = {
+        let view = UITextField()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.borderStyle = .roundedRect
+        view.textAlignment = .center
+        
+        return view
+    }()
+    
+    lazy var button: UIButton! = {
+        let view = UIButton()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.addTarget(self, action: #selector(buttonPressed) , for: .touchDown)
+        view.setTitle("Press Me!", for: .normal)
+        view.backgroundColor = UIColor.blue
+        
+        return view
+    }()
+    
+    lazy var label: UILabel! = {
+        let view = UILabel()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.text = "Hello World!"
+        view.textAlignment = .center
+        
+        return view
+    }()
+    
+    // MARK: - UITextField Delegate Methods
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        // Dismisses the Keyboard by making the text field resign
+        // first responder
+        textField.resignFirstResponder()
+        
+        // returns false. Instead of adding a line break, the text
+        // field resigns
+        return false
+    }
 }
-
